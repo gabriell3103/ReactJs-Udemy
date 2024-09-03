0261ce5af57bb108d65e37164478988d7c5f8a05
gabriell3103 <g5819853@gmail.com>
2024-09-03 16:03:10 -0300

    Inserção da page NotFound e do Search de produtos

--- 7_REACT_ROUTER/react_router/src/App.jsx
+++ 7_REACT_ROUTER/react_router/src/App.jsx
@@ -1,15 +1,21 @@
 import './App.css'
 
 // config do React Router
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+
+// components
+import NavBar from './components/NavBar';
+import SearchForm from './components/SearchForm';
 
 // pages
 import Home from './pages/Home';
 import About from './pages/About';
-
-// components
-import NavBar from './components/NavBar';
 import Product from './pages/Product';
+import Info from './pages/Info';
+import NotFound from './pages/NotFound';
+import Search from './pages/Search';
+
+
 
 function App() {
 
@@ -19,11 +25,21 @@ function App() {
       <BrowserRouter>
         {/* 2 - links com react router */}
         <NavBar />
+        {/* Search */}
+        <SearchForm />
         <Routes>
           <Route path='/' element={<Home/>}></Route>
           <Route path='/about' element={<About/>}></Route>
+          {/* 6 - nested routes */}
+          <Route path='/products/:id/info' element={<Info/>}></Route>
           {/* 4 - rota dinâmica */}
           <Route path='/products/:id' element={<Product/>}></Route>
+          {/* 9 - Search */}
+          <Route path='/search' element={<Search/>}></Route>
+          {/* 10 - Redirect */}
+          <Route path='/company' element={<Navigate to="/about"/>}></Route>
+          {/* 7 - No match route */}
+          <Route path='*' element={<NotFound/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
--- 7_REACT_ROUTER/react_router/src/components/NavBar.jsx
+++ 7_REACT_ROUTER/react_router/src/components/NavBar.jsx
@@ -1,14 +1,16 @@
 // 2 - links com react router
 
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 import "./Navbar.css";
 
 const NavBar = () => {
   return (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">Sobre</Link>
+        {/* <Link to="/">Home</Link>
+        <Link to="/about">Sobre</Link> */}
+        <NavLink to="/"  /* className={({isActive}) => (isActive ? "está-ativo" : "não-ativo")}*/>Home</NavLink>
+        <NavLink to="/about">Sobre</NavLink>
     </nav>
   )
 }
