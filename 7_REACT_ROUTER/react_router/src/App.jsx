5330652d7a321cffcd79a40ddff2fc4b73aa5fab
gabriell3103 <g5819853@gmail.com>
2024-09-03 14:44:34 -0300

    Criação de páginas e teste do router

--- 7_REACT_ROUTER/react_router/src/App.jsx
+++ 7_REACT_ROUTER/react_router/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+// config do React Router
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+// pages
+import Home from './pages/Home';
+import About from './pages/About';
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
