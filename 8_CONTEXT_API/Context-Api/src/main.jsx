8b9fa3450343e705dea991b8369b0edef535d663
gabriell3103 <g5819853@gmail.com>
2024-09-04 15:58:01 -0300

    Criando e aplicando o TitleColorContext

--- 8_CONTEXT_API/Context-Api/src/main.jsx
+++ 8_CONTEXT_API/Context-Api/src/main.jsx
@@ -4,12 +4,14 @@ import App from './App.jsx'
 import './index.css'
 
 import { CounterContextProvider } from './context/CounterContext.jsx'
-
+import {TitleColorContextProvider} from "./context/TitleColorContext.jsx"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* Criando o Provider */}
     <CounterContextProvider>
-      <App />
+      <TitleColorContextProvider>
+        <App />
+      </TitleColorContextProvider>
     </CounterContextProvider>
   </StrictMode>,
 )
